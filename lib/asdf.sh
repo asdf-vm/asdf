95d54587064600033fb78ef4a1add7d413943da2
Akash Manohar J <akash@akash.im>
2014-11-30 21:39:56 +0530

    Create the sources dir before clone a source dir

--- lib/asdf.sh
+++ lib/asdf.sh
@@ -98,6 +98,8 @@ source_add_command() {
   local package_name=$1
   local source_url=$2
   local source_path=$(get_source_path $package_name)
+
+  mkdir -p $(asdf_dir)/sources
   git clone $source_url $source_path
 }
 
