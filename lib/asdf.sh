879d8fa5f5ce90618b239de60e208a7f6f79ea26
Akash Manohar J <akash@akash.im>
2014-11-30 14:16:07 +0530

    Use binary operators

--- lib/asdf.sh
+++ lib/asdf.sh
@@ -20,7 +20,7 @@ install_command() {
   check_if_source_exists $source_path
 
   IFS=':' read -a version_info <<< "$full_version"
-  if [ "${version_info[0]}" -e "tag" ] || [ "${version_info[0]}" -e "commit" ]
+  if [ "${version_info[0]}" = "tag" ] || [ "${version_info[0]}" = "commit" ]
   then
     local install_type="${version_info[0]}"
     local version="${version_info[1]}"
