e80ec0cfbfc59a21dce92099bfee89d65671eed4
Akash Manohar J <akash@akash.im>
2015-05-14 12:21:01 +0530

    Fix sourced usage

--- asdf.sh
+++ asdf.sh
@@ -1,4 +1,7 @@
-#/usr/bin/env sh
+#!/usr/bin/env sh
 
-asdf_dir=$(cd $(dirname $0); echo $(pwd))
-export PATH="$(asdf_dir)/bin:$(asdf_dir)/shims:$PATH"
+#TODO check for ${BASH_SOURCE[0]} or use $0
+
+current_script_path=${BASH_SOURCE[0]}
+asdf_dir=$(cd $(dirname $current_script_path); echo $(pwd))
+export PATH="${asdf_dir}/bin:${asdf_dir}/shims:$PATH"
