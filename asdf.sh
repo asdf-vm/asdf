237a5ab59ee90ee46cdf16b26e59ad36f2a837a5
Akash Manohar J <akash@akash.im>
2015-05-19 10:44:08 +0530

    Fix #1: Better to stick to a single common shell
    Referred a couple other version managers. The advantage is not having to
    maintain the scripts for other shells (bash comes by default everywhere
    I've seen).

--- asdf.sh
+++ asdf.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env sh
-
-#TODO check for ${BASH_SOURCE[0]} or use $0
+#!/usr/bin/env bash
 
 current_script_path=${BASH_SOURCE[0]}
 asdf_dir=$(cd $(dirname $current_script_path); echo $(pwd))
