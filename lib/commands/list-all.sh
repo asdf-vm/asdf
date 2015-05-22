9ca6cd269fbe7ceaabbefdb8bfbd3abd8510c06e
Akash Manohar J <akash@akash.im>
2015-05-22 10:49:07 +0530

    list-all does not need the arguments

--- lib/commands/list-all.sh
+++ lib/commands/list-all.sh
@@ -3,12 +3,7 @@ list_all_command() {
   local plugin_path=$(get_plugin_path $package_name)
   check_if_plugin_exists $plugin_path
 
-  local versions=$(
-    export ASDF_INSTALL_TYPE=$install_type
-    export ASDF_INSTALL_VERSION=$version
-    export ASDF_INSTALL_PATH=$install_path
-    bash ${plugin_path}/bin/list-all
-  )
+  local versions=$(bash ${plugin_path}/bin/list-all)
 
   IFS=' ' read -a versions_list <<< "$versions"
 
