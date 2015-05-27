d037628e7e2d20e2cf2837198e51a2cc641bf7a4
Akash Manohar J <akash@akash.im>
2015-05-27 12:13:10 +0530

    Add which command

---
+++ lib/commands/which.sh
@@ -0,0 +1,15 @@
+which_command() {
+  local plugin_name=$1
+  local plugin_path=$(get_plugin_path $plugin_name)
+  check_if_plugin_exists $plugin_path
+
+  full_version=$(get_preset_version_for $plugin_name)
+
+  if [ "$full_version" == "" ]; then
+      echo "No version set for ${plugin_name}"
+      exit -1
+  else
+      echo "$full_version"
+      exit 0
+  fi
+}
