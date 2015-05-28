34222e34abb430e9fc8e6436f515f486f9b8af41
Akash Manohar J <akash@akash.im>
2015-05-29 00:08:13 +0530

    add where command to get install path

---
+++ lib/commands/where.sh
@@ -0,0 +1,25 @@
+where_command() {
+  local plugin_name=$1
+  local full_version=$2
+  local plugin_path=$(get_plugin_path $plugin_name)
+  check_if_plugin_exists $plugin_path
+
+  IFS=':' read -a version_info <<< "$full_version"
+  if [ "${version_info[0]}" = "ref" ]; then
+    local install_type="${version_info[0]}"
+    local version="${version_info[1]}"
+  else
+    local install_type="version"
+    local version="${version_info[0]}"
+  fi
+
+  local install_path=$(get_install_path $plugin_name $install_type $version)
+
+  if [ -d $install_path ]; then
+    echo $install_path
+    exit 0
+  else
+    echo "Version not installed"
+    exit 1
+  fi
+}
