bf0f9bc1fe159a90e1f978c47970b6eb1a8a25a1
Akash Manohar J <akash@akash.im>
2015-06-14 18:55:18 +0530

    Remove bulleting from plugin list

--- lib/commands/plugin-list.sh
+++ lib/commands/plugin-list.sh
@@ -3,7 +3,7 @@ plugin_list_command() {
 
   if ls $plugins_path &> /dev/null; then
     for plugin_path in $plugins_path/* ; do
-      echo "* $(basename $plugin_path)"
+      echo "$(basename $plugin_path)"
     done
   else
     echo 'Oohes nooes ~! No plugins installed'
