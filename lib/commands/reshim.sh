225c690893a7c39167d9d1d417a608bd4d5c71e8
Stratus3D <admin@stratus3d.com>
2015-11-13 11:34:16 -0500

    Remove duplicate call to write_shim_script.

--- lib/commands/reshim.sh
+++ lib/commands/reshim.sh
@@ -111,7 +111,6 @@ generate_shims_for_version() {
     for executable_file in $install_path/$bin_path/*; do
       # because just $executable_file gives absolute path; We don't want version hardcoded in shim
       local executable_path_relative_to_install_path=$bin_path/$(basename $executable_file)
-      write_shim_script $plugin_name $executable_path_relative_to_install_path
       if [ -x $executable_path_relative_to_install_path ]; then
           write_shim_script $plugin_name $executable_path_relative_to_install_path
       fi
