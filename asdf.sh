1d11a73600360d61bf7d30a524d329eeb9b54fb2
Charles Treatman <charles.treatman@gmail.com>
2016-07-01 19:40:05 -0400

    Redirect all output to /dev/null, not just stdout

--- asdf.sh
+++ asdf.sh
@@ -6,7 +6,7 @@ else
   current_script_path=$0
 fi
 
-asdf_dir=$(cd $(dirname $current_script_path) > /dev/null; echo $(pwd))
+asdf_dir=$(cd $(dirname $current_script_path) &> /dev/null; echo $(pwd))
 export PATH="${asdf_dir}/bin:${asdf_dir}/shims:$PATH"
 
 if [ -n "$ZSH_VERSION" ]; then
