ccefa996dcc8e48907a8409edfa987449f433f66
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2016-07-03 13:15:57 +0200

    plugin-update: argument validation

--- lib/commands/plugin-update.sh
+++ lib/commands/plugin-update.sh
@@ -1,4 +1,9 @@
 plugin_update_command() {
+  if [ "$#" -ne 1 ]; then
+    display_error "usage: asdf plugin-update {<name> | --all}"
+    exit 1
+  fi
+
   local plugin_name=$1
   if [ "$plugin_name" = "--all" ]; then
     for dir in $(asdf_dir)/plugins/*; do
