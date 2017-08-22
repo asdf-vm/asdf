e68e5500e68fce3772da9647a914c4f92af7ebe5
Trevor Brown <admin@stratus3d.com>
2017-08-22 16:32:41 -0400

    Create first version of release.sh tagging script.

---
+++ release.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# Unoffical Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+#ORIGINAL_IFS=$IFS
+IFS=$'\t\n' # Stricter IFS settings
+
+usage() {
+    cat <<EOF
+
+Usage: release.sh [new version]
+
+This script is only intended for use by asdf maintainers when releasing new
+versions of asdf. Plugin developers and asdf users do not need this script.
+
+This script updates the hardcoded versions in the source code and README and
+then commits them on the current branch. It then tags that commit with the
+specified version.
+
+EOF
+}
+
+error_exit() {
+    usage
+    exit 1
+}
+
+new_version="${1:-}"
+new_tag_name="v$new_version"
+
+# Make sure the user provides a version
+if [[ -z "$new_version" ]]
+then
+    echo "ERROR: no new version specified"
+    error_exit
+fi
+
+# Make sure version passed in is a plain semantic version to guard against
+# leading 'v'.
+if ! [[ "${new_version:0:1}" =~ ^[0-9]+$ ]]; then
+    echo >&2 "ERROR: semantic version should not start with a letter"
+    error_exit
+fi
+
+# Make sure the version the user provided hasn't already been tagged
+if git tag | grep "$new_tag_name" > /dev/null; then
+    echo >&2 "ERROR: git tag with that version already exists"
+    exit 1
+fi
+
+echo "INFO: Checking that all changes are commited and pushed"
+git pull
+
+# Disallow unstaged changes in the working tree
+if ! git diff-files --check --exit-code --ignore-submodules -- >&2; then
+    echo >&2 "ERROR: You have unstaged changes."
+    exit 1
+fi
+
+# Disallow uncommitted changes in the index
+if ! git diff-index --cached --exit-code -r --ignore-submodules HEAD -- >&2; then
+    echo >&2 "ERROR: Your index contains uncommitted changes."
+    exit 1
+fi
+
+# Update version in README
+sed -i.bak "s|^\(git clone.*--branch \).*$|\1$new_tag_name|" README.md
+
+# Update version in utils.sh
+# TODO: Hardcode version until.sh since not all asdf will include the Git repo
+
+# Commit the changed files before tagging the new release
+git add README.md
+git add lib/utils.sh
+git commit -m "Update version to $new_version"
+
+#git tag -fa ${VERSION} -m "Version ${VERSION}"
+#git tag -fa -a RELEASE -m "Current RELEASE"
+#
+#NEW_VERSION="${VERSION%.*}.$((${VERSION##*.}+1))"
+#set -ex
+#sed -i.bak "s/${VERSION}/${NEW_VERSION}/" setup.py
+#
+#git commit -m "Auto-increasing the version number after a release."
+
+#git push --force origin --tags
+
+echo "INFO: done."
