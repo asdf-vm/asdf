bf60c7f3ac652735c412f912bca0f67f3e2fbf51
Jonathan Knapp <jon@coffeeandcode.com>
2017-08-31 23:16:30 -0400

    Add lint script and run as part of CI tests

---
+++ lint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+shellcheck -x asdf.sh lint.sh release.sh bin/asdf bin/private/asdf-exec completions/asdf.bash test/test_helpers.bash test/fixtures/dummy_plugin/bin/* # lib/utils.sh lib/commands/*.sh
