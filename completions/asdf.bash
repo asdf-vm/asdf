df66739fe145da13631d0b86ae2b3a55f958cb42
Trevor Brown <admin@stratus3d.com>
2017-12-27 10:45:16 -0500

    Ignore new shellcheck warnings in bash completions function.

--- completions/asdf.bash
+++ completions/asdf.bash
@@ -10,21 +10,29 @@ _asdf () {
   local plugins
   plugins=$(asdf plugin-list | tr '\n' ' ')
 
+  # We can safely ignore warning SC2207 since it warns that it will uses the
+  # shell's sloppy word splitting and globbing. The possible commands here are
+  # all single words, and most likely won't contain special chars the shell will
+  # expand.
   COMPREPLY=()
 
   case "$cmd" in
     plugin-update)
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$plugins --all" -- "$cur"))
       ;;
     plugin-remove|current|list|list-all)
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$plugins" -- "$cur"))
       ;;
     install)
       if [[ "$plugins" == *"$prev"* ]] ; then
         local versions
         versions=$(asdf list-all "$prev")
+        # shellcheck disable=SC2207
         COMPREPLY=($(compgen -W "$versions" -- "$cur"))
       else
+        # shellcheck disable=SC2207
         COMPREPLY=($(compgen -W "$plugins" -- "$cur"))
       fi
       ;;
@@ -32,13 +40,16 @@ _asdf () {
       if [[ "$plugins" == *"$prev"* ]] ; then
         local versions
         versions=$(asdf list "$prev")
+        # shellcheck disable=SC2207
         COMPREPLY=($(compgen -W "$versions" -- "$cur"))
       else
+        # shellcheck disable=SC2207
         COMPREPLY=($(compgen -W "$plugins" -- "$cur"))
       fi
       ;;
     *)
       local cmds='plugin-add plugin-list plugin-remove plugin-update install uninstall update current where which list list-all local global reshim'
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$cmds" -- "$cur"))
       ;;
   esac
