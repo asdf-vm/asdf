0f48c1861bd2efab373e33177691cd69a280c549
Trevor Brown <admin@stratus3d.com>
2018-02-11 15:12:31 -0500

    Remove unnecessary regex from replacement string in regex command.

--- lib/commands/version_commands.sh
+++ lib/commands/version_commands.sh
@@ -25,7 +25,7 @@ version_command() {
   done
 
   if [ -f "$file" ] && grep "^$plugin " "$file" > /dev/null; then
-    sed -i.bak -e "s/^$plugin .*$/^$plugin ${versions[*]}$/" "$file"
+    sed -i.bak -e "s/^$plugin .*$/$plugin ${versions[*]}/" "$file"
     rm "$file".bak
   else
     echo "$plugin ${versions[*]}" >> "$file"
