c7a8226db57b4eccae1ac5d7f9b396e3fe5f9c53
Neil Martinsen-Burrell <neil@moz.com>
2018-03-30 12:09:56 -0500

    Print to stderr one level up to make tests pass

--- lib/commands/current.sh
+++ lib/commands/current.sh
@@ -16,7 +16,7 @@ plugin_current_command() {
   check_for_deprecated_plugin "$plugin_name"
 
   if [ -z "$version" ]; then
-    printf "%s\\n" "$(display_no_version_set "$plugin_name")" >&2
+    printf "%s\\n" "$(display_no_version_set "$plugin_name")"
     exit 1
   else
     printf "%-8s%s\\n" "$version" "(set by $version_file_path)"
@@ -26,7 +26,7 @@ plugin_current_command() {
 current_command() {
   if [ $# -eq 0 ]; then
     for plugin in $(plugin_list_command); do
-      printf "%-15s%s\\n" "$plugin" "$(plugin_current_command "$plugin")"
+      printf "%-15s%s\\n" "$plugin" "$(plugin_current_command "$plugin")" >&2
     done
   else
     local plugin=$1
