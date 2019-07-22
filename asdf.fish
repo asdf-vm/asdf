0d2648c7f4fce8f60c060f6ed45615e801e1fd52
Gabe Durazo <gabe@durazo.us>
2019-07-22 18:44:32 -0500

    A different approach

--- asdf.fish
+++ asdf.fish
@@ -9,9 +9,15 @@ set -l asdf_data_dir (
 set -l asdf_bin_dirs $ASDF_DIR/bin $ASDF_DIR/shims $asdf_data_dir/shims
 
 for x in $asdf_bin_dirs
-  if test -d $x; and not contains $x $PATH
-    set PATH $x $PATH
+  if test -d $x
+    for i in (seq 1 (count $PATH))
+      if test $PATH[$i] = $x
+        set -e PATH[$i]
+        break
+      end
+    end
   end
+  set PATH $x $PATH
 end
 
 # Add function wrapper so we can export variables
