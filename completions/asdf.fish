62ef77670dc8b6337484abb2430a0b4440c99dc8
Kevin Lane <lane.kevin.a@gmail.com>
2019-11-22 10:37:53 -0800

    Fix fish completion to say latest returns a stable version

--- completions/asdf.fish
+++ completions/asdf.fish
@@ -99,7 +99,7 @@ complete -f -c asdf -n '__fish_asdf_needs_command' -a which -d "Display executab
 complete -f -c asdf -n '__fish_asdf_using_command which; and __fish_asdf_arg_number 2' -a '(__fish_asdf_list_shims)'
 
 # latest completion
-complete -f -c asdf -n '__fish_asdf_needs_command' -a latest -d "Show latest available version of a package"
+complete -f -c asdf -n '__fish_asdf_needs_command' -a latest -d "Show latest stable version of a package"
 complete -f -c asdf -n '__fish_asdf_using_command latest; and __fish_asdf_arg_number 2' -a '(__fish_asdf_plugin_list)'
 
 # list completion
