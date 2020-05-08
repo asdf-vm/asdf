828ee1a0ee62c8dc7618029c3a5e8bc25d3f505b
Thomas B Homburg <thomas@madethis.dk>
2020-05-08 12:58:29 +0200

    asdf plugin list all: poxis aligned columns
    Print aligned columns without non-posix
    column command

--- lib/commands/command-plugin-list-all.bash
+++ lib/commands/command-plugin-list-all.bash
@@ -10,17 +10,23 @@ plugin_list_all_command() {
   plugins_local_path="$(get_plugin_path)"
 
   if ls "$plugins_index_path" &>/dev/null; then
-    (
+      list=""
+      width=0
       for index_plugin in "$plugins_index_path"/*; do
         index_plugin_name=$(basename "$index_plugin")
+        l=$(echo -n "$index_plugin_name" | wc -m)
+        width=$((l > width ? l : width))
+        list="$list $index_plugin_name"
+      done
+
+      for index_plugin_name in $list; do
         source_url=$(get_plugin_source_url "$index_plugin_name")
         installed_flag=" "
 
         [[ -d "${plugins_local_path}/${index_plugin_name}" ]] && installed_flag='*'
 
-        printf "%s\\t%s\\n" "$index_plugin_name" "$installed_flag$source_url"
+        printf "%-${width}s  %s\\n" "$index_plugin_name" "$installed_flag$source_url"
       done
-    ) | column -t -s $'\t'
   else
     printf "%s%s\\n" "error: index of plugins not found at " "$plugins_index_path"
   fi
