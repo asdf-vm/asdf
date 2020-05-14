9c82d11a82f1565a4f22205217f206c9c6e9ef46
Kevin Lane <lane.kevin.a@gmail.com>
2020-05-14 07:39:19 -0700

    Add support for filtering installed versions with asdf list

--- lib/commands/command-list.bash
+++ lib/commands/command-list.bash
@@ -2,6 +2,7 @@
 
 list_command() {
   local plugin_name=$1
+  local query=$2
 
   if [ -z "$plugin_name" ]; then
     local plugins_path
@@ -11,20 +12,25 @@ list_command() {
       for plugin_path in "$plugins_path"/*; do
         plugin_name=$(basename "$plugin_path")
         echo "$plugin_name"
-        display_installed_versions "$plugin_name"
+        display_installed_versions "$plugin_name" "$query"
       done
     else
       printf "%s\\n" 'Oohes nooes ~! No plugins installed'
     fi
   else
     check_if_plugin_exists "$plugin_name"
-    display_installed_versions "$plugin_name"
+    display_installed_versions "$plugin_name" "$query"
   fi
 }
 
 display_installed_versions() {
   local versions
+  local query=$2
   versions=$(list_installed_versions "$1")
+
+  if [[ $query ]]; then
+    versions=$(echo "$versions" | grep -E "^\s*$query")
+  fi
 
   if [ -n "${versions}" ]; then
     for version in $versions; do
