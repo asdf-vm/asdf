a6462655bc4ba7ebe97d5bde39c11c02e189c758
James Hegedus <james@jthegedus.com>
2020-06-10 18:44:23 +1000

    fix: log format, git checkout and use $HOME over tilde

--- install.bash
+++ install.bash
@@ -6,14 +6,15 @@ set -euo pipefail
 #ORIGINAL_IFS=$IFS
 IFS=$'\t\n' # Stricter IFS settings
 
-asdf_install_dir=${ASDF_INSTALL_DIR:-"~/.asdf"}
+asdf_install_dir=${ASDF_INSTALL_DIR:-"$HOME/.asdf"}
 
 git clone https://github.com/asdf-vm/asdf.git "${asdf_install_dir}"
-# checkout latest tag
-git --git-dir "${asdf_install_dir}" checkout "$(git --git-dir "${asdf_install_dir}" describe --abbrev=0 --tags)"
+# checkout latest tag in repo in another dir
+# credit: https://stackoverflow.com/a/6073628/7911479 and https://stackoverflow.com/a/31811385/7911479
+git --git-dir "${asdf_install_dir}/.git" --work-tree "${asdf_install_dir}" checkout "$(git describe --abbrev=0 --tags)" --quiet
 
-printf "asdf setup"
-printf "1: install asdf - completed!"
-printf "2: add asdf to your shell - https://asdf-vm.com/#/core-manage-asdf-vm"
-printf "3: add a plugin - https://asdf-vm.com/#/core-manage-plugins"
-printf "4: install a tool version - https://asdf-vm.com/#/core-manage-versions"
+printf "\n%s\n" "asdf setup"
+printf "%s\t\t\t%s\n" "1: install asdf" "completed!"
+printf "%s\t%s\n" "2: add asdf to your shell" "https://asdf-vm.com/#/core-manage-asdf-vm"
+printf "%s\t\t\t%s\n" "3: add a plugin" "https://asdf-vm.com/#/core-manage-plugins"
+printf "%s\t%s\n" "4: install a tool version" "https://asdf-vm.com/#/core-manage-versions"
