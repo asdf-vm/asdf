c6d0b3f799ce1e77dee60b9d2ed98f5f35d72d97
Trevor Brown <admin@stratus3d.com>
2020-07-09 10:27:19 -0400

    Add support for the plugin documentation callback

--- lib/commands/command-help.bash
+++ lib/commands/command-help.bash
@@ -21,19 +21,74 @@ asdf_extension_cmds() {
   # shellcheck disable=SC2012
   ext_cmds="$(ls -1 "$plugins_path"/*/lib/commands/command*.bash 2>/dev/null |
     sed "s#^$plugins_path/##;s#lib/commands/command##;s/.bash//;s/^-//;s/-/ /g")"
-  if test -n "$ext_cmds"; then
-    echo "$ext_cmds" | cut -d'/' -f 1 | uniq | while read -r plugin; do
-      echo
-      echo "PLUGIN $plugin"
-      echo "$ext_cmds" | grep "$plugin/" | sed "s#^$plugin/#  asdf $plugin#" | sort
-    done
-  fi
-}
+      if test -n "$ext_cmds"; then
+        echo "$ext_cmds" | cut -d'/' -f 1 | uniq | while read -r plugin; do
+        echo
+        echo "PLUGIN $plugin"
+        echo "$ext_cmds" | grep "$plugin/" | sed "s#^$plugin/#  asdf $plugin#" | sort
+      done
+      fi
+    }
 
-help_command() {
-  asdf_help
-  asdf_extension_cmds
-  asdf_moto
+  help_command() {
+    local plugin_name="$1"
+    local tool_version="$2"
+    local plugin_path
+
+  # If plugin name is present as first argument output plugin help info
+  if [ -n "$plugin_name" ]; then
+    plugin_path=$(get_plugin_path "$plugin_name")
+
+    if [ -d "$plugin_path" ]; then
+      if [ -f "${plugin_path}/bin/download" ]; then
+        if [ -n "$tool_version" ]; then
+
+          # TODO: Refactor this code out into helper functions in utils.bash
+          IFS=':' read -r -a version_info <<<"$tool_version"
+          if [ "${version_info[0]}" = "ref" ]; then
+            local install_type="${version_info[0]}"
+            local version="${version_info[1]}"
+          else
+            local install_type="version"
+
+            if [ "${version_info[0]}" = "latest" ]; then
+              local version
+              version=$(asdf latest "$plugin_name" "${version_info[1]}")
+              full_version=$version
+            else
+              local version="${version_info[0]}"
+            fi
+          fi
+
+          local install_path
+          install_path=$(get_install_path "$plugin_name" "$install_type" "$version")
+
+          (
+          # shellcheck disable=SC2031
+          export ASDF_INSTALL_TYPE=$install_type
+          # shellcheck disable=SC2031
+          export ASDF_INSTALL_VERSION=$version
+          # shellcheck disable=SC2031
+          export ASDF_INSTALL_PATH=$install_path
+          bash "${plugin_path}"/bin/documentation
+        )
+      else
+        (bash "${plugin_path}"/bin/documentation)
+        fi
+      else
+        echo "No documentation for plugin $plugin_name" >&2
+        exit 1
+      fi
+    else
+      echo "No plugin named $plugin_name" >&2
+      exit 1
+    fi
+  else
+    # Otherwise output general asdf help
+    asdf_help
+    asdf_extension_cmds
+    asdf_moto
+  fi
 }
 
 help_command "$@"
---
+++ test/help_command.bats
@@ -0,0 +1,76 @@
+#!/usr/bin/env bats
+
+load test_helpers
+
+setup() {
+  setup_asdf_dir
+  install_dummy_plugin
+  install_dummy_legacy_plugin
+  run asdf install dummy 1.0
+  run asdf install dummy 1.1
+
+  PROJECT_DIR=$HOME/project
+  mkdir $PROJECT_DIR
+}
+
+teardown() {
+  clean_asdf_dir
+}
+
+@test "help should show dummy plugin help" {
+  cd $PROJECT_DIR
+
+  run asdf help "dummy"
+
+  expected_output="$(cat <<EOF
+Dummy plugin documentation
+
+Dummy plugin is a plugin only used for unit tests
+EOF
+)"
+  [ "$status" -eq 0 ]
+  [ "$output" = "$expected_output" ]
+}
+
+@test "help should show dummy plugin help specific to version when version is present" {
+  cd $PROJECT_DIR
+
+  run asdf help "dummy" "1.2.3"
+
+  expected_output="$(cat <<EOF
+Dummy plugin documentation
+
+Dummy plugin is a plugin only used for unit tests
+
+Details specific for version 1.2.3
+EOF
+)"
+  [ "$status" -eq 0 ]
+  echo $output
+  [ "$output" = "$expected_output" ]
+}
+
+@test "help should fail for unknown plugins" {
+  cd $PROJECT_DIR
+
+  run asdf help "sunny"
+  [ "$status" -eq 1 ]
+  [ "$output" == "No plugin named sunny" ]
+}
+
+@test "help should fail when plugin doesn't have documentation callback" {
+  cd $PROJECT_DIR
+
+  run asdf help "legacy-dummy"
+  [ "$status" -eq 1 ]
+  [ "$output" == "No documentation for plugin legacy-dummy" ]
+}
+
+@test "help should show asdf help when no plugin name is provided" {
+  cd $PROJECT_DIR
+
+  run asdf help
+
+  [ "$status" -eq 0 ]
+  # TODO: Assert asdf help output is printed
+}
