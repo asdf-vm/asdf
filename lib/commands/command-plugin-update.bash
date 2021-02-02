2baea65f148d15a9e59fe5c6407fd76e3c0a862c
jthegedus <jthegedus@hey.com>
2021-02-02 13:22:23 +1100

    fix: detect default branch instead of branch of current sha

--- lib/commands/command-plugin-update.bash
+++ lib/commands/command-plugin-update.bash
@@ -7,39 +7,31 @@ plugin_update_command() {
   fi
 
   local plugin_name="$1"
-  local plugin_path
-  local branch_of_head
-  local gitref
+  local gitref="${2}"
 
   if [ "$plugin_name" = "--all" ]; then
     for dir in "$(asdf_data_dir)"/plugins/*; do
-      branch_of_head=$(git --git-dir "$dir/.git" rev-parse --abbrev-ref HEAD 2>/dev/null)
-
-      printf "%s %s%s\\n" "Updating" "$(basename "$dir")" "..."
-      gitref="${branch_of_head}"
-      git --git-dir "$dir/.git" --work-tree "$dir" fetch -p -u origin "$gitref:$gitref"
-      git --git-dir "$dir/.git" --work-tree "$dir" checkout -f "$gitref"
+      update_plugin "$(basename "$dir")" "$dir" "$gitref" &
     done
+    wait
   else
-    check_if_plugin_exists "$plugin_name"
+    local plugin_path
     plugin_path="$(get_plugin_path "$plugin_name")"
-    branch_of_head=$(git --git-dir "$plugin_path/.git" rev-parse --abbrev-ref HEAD 2>/dev/null)
-
-    printf "%s %s%s\\n" "Updating" "$plugin_name" "..."
-    gitref="${2:-${branch_of_head}}"
-    git --git-dir "$plugin_path/.git" --work-tree "$plugin_path" fetch -p -u origin "$gitref:$gitref"
-    git --git-dir "$plugin_path/.git" --work-tree "$plugin_path" checkout -f "$gitref"
+    check_if_plugin_exists "$plugin_name"
+    update_plugin "$plugin_name" "$plugin_path" "$gitref"
   fi
 }
 
 update_plugin() {
   local plugin_name=$1
   local plugin_path=$2
-  local gitref=$3
+  repo_default_branch=$(git --git-dir "$plugin_path/.git" --work-tree "$plugin_path" remote show origin | grep 'HEAD branch' | cut -d' ' -f5)
+  local gitref=${3:-${repo_default_branch}}
   logfile=$(mktemp)
   {
     printf "Updating %s...\\n" "$plugin_name"
-    (cd "$plugin_path" && git fetch -p -u origin "$gitref:$gitref" && git checkout -f "$gitref")
+    git --git-dir "$plugin_path/.git" --work-tree "$plugin_path" fetch --prune --update-head-ok origin "$gitref:$gitref"
+    git --git-dir "$plugin_path/.git" --work-tree "$plugin_path" checkout --force "$gitref"
   } >"$logfile" 2>&1
   cat "$logfile"
   rm "$logfile"
