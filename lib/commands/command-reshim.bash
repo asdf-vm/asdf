43d6fef000a2d7ab5ab2cc662f7d066d3489e8b1
jthegedus <jthegedus@hey.com>
2021-04-27 19:31:22 +1000

    fix: format command-reshim

--- lib/commands/command-reshim.bash
+++ lib/commands/command-reshim.bash
@@ -125,10 +125,10 @@ remove_obsolete_shims() {
   # comm only takes to files, so we write this data to temp files so we can
   # pass it to comm.
   formatted_shims=$(mktemp /tmp/asdf-command-reshim-formatted-shims.XXXXXX)
-  printf "%s\\n" "$shims" > "$formatted_shims"
+  printf "%s\\n" "$shims" >"$formatted_shims"
 
   formatted_exec_names=$(mktemp /tmp/asdf-command-reshim-formatted-exec-names.XXXXXX)
-  printf "%s\\n" "$exec_names" > "$formatted_exec_names"
+  printf "%s\\n" "$exec_names" >"$formatted_exec_names"
 
   obsolete_shims=$(comm -23 "$formatted_shims" "$formatted_exec_names")
   rm -f "$formatted_exec_names" "$formatted_shims"
