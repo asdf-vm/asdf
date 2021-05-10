d4e09ca23d837f1e8f3ef6a4fdb32894afa9fa8c
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2021-05-10 14:03:03 +0200

    feat: asdf latest defer to plugin to determine the latest version
    If the command `latest-stable` exists in the plugin, `asdf latest`
    defers to it to determine the latest stable version.
    
    Fixes https://github.com/asdf-vm/asdf/issues/763

--- lib/commands/command-latest.bash
+++ lib/commands/command-latest.bash
@@ -5,8 +5,17 @@ latest_command() {
 
   local plugin_name=$1
   local query=$2
+  local plugin_path
+
+  plugin_path=$(get_plugin_path "$plugin_name")
+  check_if_plugin_exists "$plugin_name"
 
   [[ -z $query ]] && query="$DEFAULT_QUERY"
+
+  if [ -f "${plugin_path}/bin/latest-stable" ]; then
+    bash "${plugin_path}"/bin/latest-stable "$query"
+    return
+  fi
 
   # pattern from xxenv-latest (https://github.com/momo-lab/xxenv-latest)
   asdf list-all "$plugin_name" "$query" |
