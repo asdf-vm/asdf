fd842ad74d654d09f9e86e89094f5b20f0c6a45c
jthegedus <jthegedus@hey.com>
2021-05-25 15:55:55 +1000

    ci: extract linting to own worflow

--- lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-exec shellcheck -s bash -x \
-  asdf.sh lint.sh release/tag.sh \
-  bin/asdf bin/private/asdf-exec \
-  lib/utils.bash lib/commands/*.bash \
-  completions/*.bash \
-  test/test_helpers.bash \
-  test/fixtures/dummy_plugin/bin/*
