0613ad4788dc052f863fe8f751f09ee652961270
jthegedus <jthegedus@hey.com>
2021-07-07 11:24:24 +1000

    fix: simplify by suppress list-all stderr

--- lib/commands/command-latest.bash
+++ lib/commands/command-latest.bash
@@ -7,28 +7,29 @@ latest_command() {
   local query=$2
   local plugin_path
 
+  [[ -z $query ]] && query="$DEFAULT_QUERY"
+
   plugin_path=$(get_plugin_path "$plugin_name")
   check_if_plugin_exists "$plugin_name"
 
-  [[ -z $query ]] && query="$DEFAULT_QUERY"
+  local versions
 
   if [ -f "${plugin_path}/bin/latest-stable" ]; then
-    bash "${plugin_path}"/bin/latest-stable "$query"
+    versions=$(bash "${plugin_path}"/bin/latest-stable "$query")
   else
-    local versions
     # pattern from xxenv-latest (https://github.com/momo-lab/xxenv-latest)
     versions=$(asdf list-all "$plugin_name" "$query" |
       grep -vE "(^Available versions:|-src|-dev|-latest|-stm|[-\\.]rc|-alpha|-beta|[-\\.]pre|-next|(a|b|c)[0-9]+|snapshot|master)" |
       sed 's/^\s\+//' |
-      tail -1)
-
-    if [ -z "${versions}" ]; then
-      exit 1
-    fi
-
-    printf "%s\n" "$versions"
-
+      tail -1) 2>/dev/null
   fi
+
+  if [ -z "${versions}" ]; then
+    display_error "No compatible versions available ($plugin_name $query)"
+    exit 1
+  fi
+
+  printf "%s\n" "$versions"
 }
 
 latest_command "$@"
