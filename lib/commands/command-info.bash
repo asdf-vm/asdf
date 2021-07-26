cd0a6a779eb18236fe7bf1f84403e33e636ef1f3
James Hegedus <jthegedus@hey.com>
2021-07-27 08:57:13 +1000

    feat: display plugin repo refs alongside urls in info cmd (#1014)

--- lib/commands/command-info.bash
+++ lib/commands/command-info.bash
@@ -5,7 +5,7 @@ info_command() {
   printf "%s:\\n%s\\n\\n" "SHELL" "$($SHELL --version)"
   printf "%s:\\n%s\\n\\n" "ASDF VERSION" "$(asdf_version)"
   printf "%s:\\n%s\\n\\n" "ASDF ENVIRONMENT VARIABLES" "$(env | grep -E "ASDF_DIR|ASDF_DATA_DIR|ASDF_CONFIG_FILE|ASDF_DEFAULT_TOOL_VERSIONS_FILENAME")"
-  printf "%s:\\n%s\\n\\n" "ASDF INSTALLED PLUGINS" "$(asdf plugin list --urls)"
+  printf "%s:\\n%s\\n\\n" "ASDF INSTALLED PLUGINS" "$(asdf plugin list --urls --refs)"
 }
 
 info_command "$@"
