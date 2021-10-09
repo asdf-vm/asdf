242b88d6f182b5141dbc150a78f0021e7daba848
Elijah Shaw-Rutschman <elijahr@gmail.com>
2021-10-09 01:16:57 -0500

    fix shellcheck issues

--- lib/commands/command-export-shell-version.bash
+++ lib/commands/command-export-shell-version.bash
@@ -27,7 +27,7 @@ shell_command() {
       # Elvish doesn't have a `source` command, and eval is banned, so the
       # var name and value are printed on separate lines for asdf.elv to parse
       # and pass to unset-env.
-      printf "unset-env\n$version_env_var"
+      printf "unset-env\n%s" "$version_env_var"
       ;;
     *)
       printf "unset %s\\n" "$version_env_var"
@@ -52,7 +52,7 @@ shell_command() {
     # Elvish doesn't have a `source` command, and eval is banned, so the
     # var name and value are printed on separate lines for asdf.elv to parse
     # and pass to set-env.
-    printf "set-env\n$version_env_var\n$version"
+    printf "set-env\n%s\n%s" "$version_env_var" "$version"
     ;;
   *)
     printf "export %s=\"%s\"\\n" "$version_env_var" "$version"
