40324a7959f7c360090191bcc545f22bd93c4843
Philippe Grégoire <git@pgregoire.xyz>
2021-12-01 00:16:31 -0500

    rewrite asdf function to not rely on the "local" keyword
    The `asdf` function makes use of the `local` keyword which is not
    supported by all shells. A simple rewrite of the function allows to
    eliminate the use of a (local) variable to store the first argument
    and, as a consequence, removes the need for the `local` keyword.

--- lib/asdf.sh
+++ lib/asdf.sh
@@ -1,19 +1,14 @@
 # The asdf function is a wrapper so we can export variables
 asdf() {
-  local command
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
-
-  case "$command" in
+  case "$1" in
   "shell")
+    shift
     # commands that need to export variables
     eval "$(asdf export-shell-version sh "$@")" # asdf_allow: eval
     ;;
   *)
     # forward other commands to asdf script
-    command asdf "$command" "$@"
+    command asdf "$@"
     ;;
 
   esac
