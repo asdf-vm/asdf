c22bb3db7cba3cd374ff10ad104a680aa35edf06
Trevor Brown <admin@stratus3d.com>
2022-02-17 11:02:05 -0500

    Remove last ls command from codebase

--- lib/utils.bash
+++ lib/utils.bash
@@ -451,10 +451,7 @@ resolve_symlink() {
   local symlink
   symlink="$1"
 
-  # This seems to be the only cross-platform way to resolve symlink paths to
-  # the real file path.
-  # shellcheck disable=SC2012
-  resolved_path=$(ls -l "$symlink" | sed -e 's|.*-> \(.*\)|\1|') # asdf_allow: ls '
+  resolved_path=$(stat -f "%Y" "$symlink")
 
   # Check if resolved path is relative or not by looking at the first character.
   # If it is a slash we can assume it's root and absolute. Otherwise we treat it
