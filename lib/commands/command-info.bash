b1e3e4ffd39373743c261cce8cee39fa0e635358
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-04-27 15:22:36 +1000

    fix: more robust SHELL checks
    While debugging an unrelated issue, I found that my `$SHELL --version`
    output was returning as zsh but I actually run `fish` via nix. Instead
    of using `$SHELL`, which isn't accurate in some cases, I've updated this
    to attempt to use the various `*_VERSION` environment variables that are
    available in the shells before failing back to the previous `$SHELL`
    check. Realistically, `$SHELL --version` should be replaced with
    something else but this should improve the current situation with
    misidentifying shells in use.

--- lib/commands/command-info.bash
+++ lib/commands/command-info.bash
@@ -2,9 +2,21 @@
 # shellcheck source=lib/functions/plugins.bash
 . "$(dirname "$(dirname "$0")")/lib/functions/plugins.bash"
 
+get_shell() {
+  if [[ -z $FISH_VERSION ]]; then
+    echo "fish"
+  elif [[ -z $ZSH_VERSION ]]; then
+    echo "zsh"
+  elif [[ -z $BASH_VERSION ]]; then
+    echo "bash"
+  else
+    echo $SHELL --version
+  fi
+}
+
 info_command() {
   printf "%s:\\n%s\\n\\n" "OS" "$(uname -a)"
-  printf "%s:\\n%s\\n\\n" "SHELL" "$($SHELL --version)"
+  printf "%s:\\n%s\\n\\n" "SHELL" "$(get_shell)"
   printf "%s:\\n%s\\n\\n" "ASDF VERSION" "$(asdf_version)"
   printf "%s:\\n%s\\n\\n" "ASDF ENVIRONMENT VARIABLES" "$(env | grep -E "ASDF_DIR|ASDF_DATA_DIR|ASDF_CONFIG_FILE|ASDF_DEFAULT_TOOL_VERSIONS_FILENAME")"
   printf "%s:\\n%s\\n\\n" "ASDF INSTALLED PLUGINS" "$(plugin_list_command --urls --refs)"
