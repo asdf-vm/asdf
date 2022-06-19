6a01c7c46c99c57083f7d941b82fa7e069535d48
Michael Weigle <michael.weigle@gmail.com>
2022-06-18 22:44:34 -0400

    dynamic spacing for current
    * adds stty
    * kind of verbose/kludgy
    * not passing all tests yet

--- lib/commands/command-current.bash
+++ lib/commands/command-current.bash
@@ -44,16 +44,73 @@ plugin_current_command() {
 
 # shellcheck disable=SC2059
 current_command() {
-  local terminal_format="%-15s %-15s %-10s\\n"
+  local terminal_format="%s %s %s\\n"
   local exit_status=0
   local plugin
+  declare -A collected_currents
+  declare -A max_widths
+  declare -A min_widths
 
-  # printf "$terminal_format" "PLUGIN" "VERSION" "SET BY CONFIG" # disable this until we release headings across the board
+  # Set initial column widths to max the terminal can support
+  local terminal_width
+  terminal_width=$(stty size | cut -d ' ' -f 2)
+  # reset if less than 80 cols, likely reading the wrong device
+  if [ "$terminal_width" -lt 80 ]; then
+    terminal_width=80
+  fi
+  # leave room for spaces
+  terminal_width=$((terminal_width - 3))
+
+  # Integer divison, but floors/truncates so we should have a couple characters left over instead of running to the next line
+  max_widths[name]=$((terminal_width / 4))
+  max_widths[version]=$((terminal_width / 4))
+  max_widths[path]=$((terminal_width * 2 / 4))
+
+  min_widths[name]=10
+  min_widths[version]=10
+  min_widths[path]=20
+
+  # disable this until we release headings across the board
+  # printf "$terminal_format" "PLUGIN" "VERSION" "SET BY CONFIG"
   if [ $# -eq 0 ]; then
     # shellcheck disable=SC2119
     for plugin in $(plugin_list_command); do
-      plugin_current_command "$plugin" "$terminal_format"
+      collected_currents[$plugin]=$(plugin_current_command "$plugin" "$terminal_format")
+
+      local plugin_name
+      plugin_name=$(cut -d ' ' -f 1 <<<"${collected_currents[$plugin]}")
+      local plugin_version
+      plugin_version=$(cut -d ' ' -f 2 <<<"${collected_currents[$plugin]}")
+      local plugin_path
+      plugin_path=$(cut -d ' ' -f 3 <<<"${collected_currents[$plugin]}")
+
+      if [ "${min_widths[name]}" -lt ${#plugin_name} ]; then
+        # printf "${plugin_name} has a long name!\\n"
+        min_widths[name]=${#plugin_name}
+      fi
+      if [ "${min_widths[version]}" -lt ${#plugin_version} ]; then
+        # printf "${plugin_name} has a long version!\\n"
+        min_widths[version]=${#plugin_version}
+      fi
+      if [ "${min_widths[path]}" -lt ${#plugin_path} ]; then
+        # printf "${plugin_name} has a long path!\\n"
+        min_widths[path]=${#plugin_path}
+      fi
     done
+
+    # make first 2 cols equal size, make second as large as it needs to if there's room?
+    terminal_format="%-${min_widths[name]}.${max_widths[name]}s  %-${min_widths[version]}.${max_widths[version]}s  %-${min_widths[path]}.${max_widths[path]}s\\n"
+
+    for plugin in "${!collected_currents[@]}"; do
+      local plugin_name
+      plugin_name=$(cut -d ' ' -f 1 <<<"${collected_currents[$plugin]}")
+      local plugin_version
+      plugin_version=$(cut -d ' ' -f 2 <<<"${collected_currents[$plugin]}")
+      local plugin_path
+      plugin_path=$(cut -d ' ' -f 3 <<<"${collected_currents[$plugin]}")
+      printf "$terminal_format" "$plugin_name" "$plugin_version" "${plugin_path}"
+    done
+
   else
     plugin=$1
     plugin_current_command "$plugin" "$terminal_format"
