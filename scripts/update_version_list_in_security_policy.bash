b085be3ea8476426dde9f205cbe852695f1e57d4
jthegedus <jthegedus@hey.com>
2022-07-12 23:55:03 +1000

    chore: use correct english in comments

--- scripts/update_version_list_in_security_policy.bash
+++ scripts/update_version_list_in_security_policy.bash
@@ -6,7 +6,7 @@ version_major_minor_x="$(cut -f1-2 -d "." version.txt).x"
 
 # skip if version is already in the list
 if ! grep -q "$version_major_minor_x" SECURITY.md; then
-  # append new version to top of list
+  # prepend new version to the list
   sed -i "s/white_check_mark:/x:               /g" SECURITY.md
   sed -i "s/^\\(| -* | -* |\\)$/\\1\\n| $version_major_minor_x  | :white_check_mark: |/" SECURITY.md
 fi
