ab3b962cd2b67c37fa05a8822ccfc29fe0b91fe2
jthegedus <jthegedus@hey.com>
2022-07-12 23:53:02 +1000

    chore: pull out version compute from sed cmd

--- scripts/update_version_in_docs.bash
+++ scripts/update_version_in_docs.bash
@@ -2,4 +2,5 @@
 
 set -euo pipefail
 
-sed -i "s/\\(git clone.*--branch \\).*\\(\`.*|\\)/\\1v$(cat version.txt)\\2/" docs/guide/getting-started.md
+version=$(cat version.txt)
+sed -i "s/\\(git clone.*--branch \\).*\\(\`.*|\\)/\\1v$version\\2/" docs/guide/getting-started.md
