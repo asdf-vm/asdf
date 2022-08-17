2c81b0c47e7b7133749ca10a1c0c1ecca5949180
Yuri Pereira Constante <ypconstante@gmail.com>
2022-08-16 23:57:53 -0300

    refactor: fallback for older fish versions

--- asdf.fish
+++ asdf.fish
@@ -2,12 +2,38 @@ if not set -q ASDF_DIR
   set -x ASDF_DIR (dirname (status -f))
 end
 
-fish_add_path --global --move $ASDF_DIR/bin
+# Add asdf to PATH
+# fish_add_path was added in fish 3.2, so we need a fallback for older version
+if type -q fish_add_path
+  fish_add_path --global --move $ASDF_DIR/bin
 
-if test -n "$ASDF_DATA_DIR"
-  fish_add_path --global --move "$ASDF_DATA_DIR/shims"
+  if test -n "$ASDF_DATA_DIR"
+    fish_add_path --global --move "$ASDF_DATA_DIR/shims"
+  else
+    fish_add_path --global --move "$HOME/.asdf/shims"
+  end
 else
-  fish_add_path --global --move "$HOME/.asdf/shims"
+  set -l asdf_user_shims (
+    if test -n "$ASDF_DATA_DIR"
+      printf "%s\n" "$ASDF_DATA_DIR/shims"
+    else
+      printf "%s\n" "$HOME/.asdf/shims"
+    end
+  )
+
+  set -l asdf_bin_dirs $ASDF_DIR/bin $asdf_user_shims
+
+  for x in $asdf_bin_dirs
+    if test -d $x
+      for i in (seq 1 (count $PATH))
+        if test $PATH[$i] = $x
+          set -e PATH[$i]
+          break
+        end
+      end
+    end
+    set PATH $x $PATH
+  end
 end
 
 # Load the asdf wrapper function
