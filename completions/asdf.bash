5868ff9bb9b981dd453d2a311e5abb960057bb3a
Ville Skyttä <ville.skytta@iki.fi>
2022-09-07 07:50:19 +0300

    feat(completions): add bash `plugin-list/-all`, `info` non-completions

--- completions/asdf.bash
+++ completions/asdf.bash
@@ -75,6 +75,7 @@ _asdf() {
     # shellcheck disable=SC2207
     COMPREPLY=($(compgen -c -- "$cur"))
     ;;
+  plugin-list | plugin-list-all | info) ;;
   *)
     local cmds='current global help install list list-all local plugin-add plugin-list plugin-list-all plugin-remove plugin-update reshim shell uninstall update where which info'
     # shellcheck disable=SC2207
