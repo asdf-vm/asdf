7fa3947e4d09ed7cd71a586bdee71fa2179ef06b
Yusup Hambali <supalpuket@gmail.com>
2022-10-18 13:30:24 +0700

    Fix the wrong path

--- lib/commands/command-plugin-add.bash
+++ lib/commands/command-plugin-add.bash
@@ -1,5 +1,5 @@
 # -*- sh -*-
 # shellcheck source=lib/functions/plugins.bash
-. "$(dirname "$(dirname "$0")")/lib/functions/plugins.bash"
+. "$(dirname "$(dirname "$0")")/functions/plugins.bash"
 
 plugin_add_command "$@"
