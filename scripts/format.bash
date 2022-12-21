43b461de06e01afb07d7767aba76674bcec10c1b
jthegedus <jthegedus@hey.com>
2022-12-21 16:30:37 +1100

    chore: format script for local formatting

---
+++ scripts/format.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# check .sh files
+shfmt --language-dialect posix --indent 2 --write \
+  lib/*.sh
+
+# check .bash files
+shfmt --language-dialect bash --indent 2 --write \
+  completions/*.bash \
+  bin/asdf \
+  bin/private/asdf-exec \
+  lib/utils.bash \
+  lib/commands/*.bash \
+  lib/functions/*.bash \
+  scripts/*.bash \
+  test/test_helpers.bash \
+  test/fixtures/dummy_broken_plugin/bin/* \
+  test/fixtures/dummy_legacy_plugin/bin/* \
+  test/fixtures/dummy_plugin/bin/*
+
+# check .bats files
+shfmt --language-dialect bats --indent 2 --write \
+  test/*.bats
