18c9c6632e25976b5065117988ab80372794da6f
jthegedus <jthegedus@hey.com>
2023-01-29 22:10:04 +1100

    chore: error in CI if non-parallel execution

--- scripts/test.bash
+++ scripts/test.bash
@@ -2,9 +2,17 @@
 
 set -euo pipefail
 
-bats \
-  --timing \
-  --jobs 2 \
-  --no-parallelize-within-files \
-  --print-output-on-failure \
-  test
+bats_options=(--timing --print-output-on-failure)
+
+if command -v parallel; then
+  # enable parallel jobs
+  bats_options=("${bats_options[@]}" --jobs 2 --no-parallelize-within-files)
+elif [[ -n "${CI-}" ]]; then
+  printf "%s\n" "GNU parallel should be installed in the CI environment. Please install and rerun the test suite."
+  exit 1
+else
+  printf "%s\n" "For faster test execution, install GNU parallel."
+fi
+
+bats "${bats_options[@]}" \
+  ./test
