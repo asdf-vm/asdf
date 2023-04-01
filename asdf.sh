00fee78423de0e399f5705bb483e599e39b707c9
Edwin Kofler <edwin@kofler.dev>
2023-03-31 20:22:24 -0700

    fix!: Remove files containing only `asdf` wrapper functions (#1525)

--- asdf.sh
+++ asdf.sh
@@ -70,6 +70,21 @@ esac
 
 unset -v _asdf_bin _asdf_shims
 
-# shellcheck source=lib/asdf.sh
-. "$ASDF_DIR/lib/asdf.sh"
+# The asdf function is a wrapper so we can export variables
+asdf() {
+  case $1 in
+  "shell")
+    if ! shift; then
+      printf '%s\n' 'asdf: Error: Failed to shift' >&2
+      return 1
+    fi
 
+    # Invoke command that needs to export variables.
+    eval "$(asdf export-shell-version sh "$@")" # asdf_allow: eval
+    ;;
+  *)
+    # Forward other commands to asdf script.
+    command asdf "$@" # asdf_allow: ' asdf '
+    ;;
+  esac
+}
