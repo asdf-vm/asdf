b40beb6039b031d350bd1c621f34d90f23f72765
Trevor Brown <admin@stratus3d.com>
2024-02-21 19:52:34 -0500

    feat(golang-rewrite): add placeholders for plugin subcommands
    * Switch from cobra to urfave/cli
    * Create placeholder plugin command structs

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,83 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
 	"os"
 
-	"github.com/spf13/cobra"
+	"github.com/urfave/cli/v2"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "asdf",
-	Short: "The multiple runtime version manager",
-	Long: `The Multiple Runtime Version Manager.
+const usageText = `The Multiple Runtime Version Manager.
 
 Manage all your runtime versions with one tool!
 
-Complete documentation is available at https://asdf-vm.com/`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Flesh this out
-		fmt.Println("Late but latest -- Rajinikanth")
-	},
-}
-
-func init() {
-	// TODO: Add flags relevant to all commands
-	//rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
-
-	// TODO: Add sub commands
-	//rootCmd.AddCommand(pluginCmd)
-}
+Complete documentation is available at https://asdf-vm.com/`
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	app := &cli.App{
+		Name:    "asdf",
+		Version: "0.1.0",
+		// Not really sure what I should put here, but all the new Golang code will
+		// likely be written by me.
+		Copyright: "(c) 2024 Trevor Brown",
+		Authors: []*cli.Author{
+			&cli.Author{
+				Name: "Trevor Brown",
+			},
+		},
+		Usage:     "The multiple runtime version manager",
+		UsageText: usageText,
+		Commands: []*cli.Command{
+			// TODO: Flesh out all these commands
+			&cli.Command{
+				Name: "plugin",
+				Action: func(cCtx *cli.Context) error {
+					log.Print("Foobar")
+					return nil
+				},
+				Subcommands: []*cli.Command{
+					&cli.Command{
+						Name: "add",
+						Action: func(cCtx *cli.Context) error {
+							log.Print("Baz")
+							return nil
+						},
+					},
+					&cli.Command{
+						Name: "list",
+						Action: func(cCtx *cli.Context) error {
+							log.Print("Bim")
+							return nil
+						},
+					},
+					&cli.Command{
+						Name: "Lorem",
+						Action: func(cCtx *cli.Context) error {
+							log.Print("Foobar")
+							return nil
+						},
+					},
+					&cli.Command{
+						Name: "update",
+						Action: func(cCtx *cli.Context) error {
+							log.Print("Ipsum")
+							return nil
+						},
+					},
+				},
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			// TODO: flesh this out
+			log.Print("Late but latest -- Rajinikanth")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+
+	if err != nil {
 		os.Exit(1)
+		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,12 +5,13 @@ go 1.21.5
 require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/sethvargo/go-envconfig v1.0.0
-	github.com/spf13/cobra v1.8.0
+	github.com/urfave/cli/v2 v2.27.1
 	gopkg.in/ini.v1 v1.67.0
 )
 
 require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 )
