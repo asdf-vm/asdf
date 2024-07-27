f74efbf1bff5d7489f86f2f568eba2667d95645e
Trevor Brown <admin@stratus3d.com>
2024-07-27 17:20:59 -0400

    feat(golang-rewrite): offline Go tests
    Some of the Go tests actually cloned remote repos, which meant slow tests, and tests failures if I was working offline. These changes allow all Go tests to run offline. The test code now clones local Git repos instead of remotes ones.
    
    * Add otiai10/copy as a dependency
    * Create repotest package for test code that needs to work with Git repos
    * Add back missing test script for execute package
    * Update git, pluginindex, and plugins package tests so they work offline

--- plugins/plugins_test.go
+++ plugins/plugins_test.go
@@ -1,28 +1,23 @@
 package plugins
 
 import (
-	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 	"testing"
 
 	"asdf/config"
+	"asdf/repotest"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// TODO: Switch to local repo so tests don't go over the network
-const (
-	testRepo       = "https://github.com/Stratus3D/asdf-lua"
-	testPluginName = "lua"
-)
+const testPluginName = "lua"
 
 func TestList(t *testing.T) {
 	testDataDir := t.TempDir()
 	conf := config.Config{DataDir: testDataDir}
-	testRepo, err := installMockPluginRepo(testDataDir, testPluginName)
+	testRepo, err := repotest.GeneratePlugin("dummy_plugin", testDataDir, testPluginName)
 	assert.Nil(t, err)
 
 	err = Add(conf, testPluginName, testRepo)
@@ -93,7 +88,7 @@ func TestAdd(t *testing.T) {
 
 		for _, invalid := range invalids {
 			t.Run(invalid, func(t *testing.T) {
-				err := Add(config.Config{}, invalid, testRepo)
+				err := Add(config.Config{}, invalid, "never-cloned")
 
 				expectedErrMsg := "is invalid. Name may only contain lowercase letters, numbers, '_', and '-'"
 				if !strings.Contains(err.Error(), expectedErrMsg) {
@@ -107,13 +102,16 @@ func TestAdd(t *testing.T) {
 		conf := config.Config{DataDir: testDataDir}
 
 		// Add plugin
-		err := Add(conf, testPluginName, testRepo)
+		repoPath, err := repotest.GeneratePlugin("dummy_plugin", testDataDir, testPluginName)
+		assert.Nil(t, err)
+
+		err = Add(conf, testPluginName, repoPath)
 		if err != nil {
 			t.Fatal("Expected to be able to add plugin")
 		}
 
 		// Add it again to trigger error
-		err = Add(conf, testPluginName, testRepo)
+		err = Add(conf, testPluginName, repoPath)
 
 		if err == nil {
 			t.Fatal("expected error got nil")
@@ -136,8 +134,10 @@ func TestAdd(t *testing.T) {
 	t.Run("when plugin name and URL are valid installs plugin", func(t *testing.T) {
 		testDataDir := t.TempDir()
 		conf := config.Config{DataDir: testDataDir}
+		pluginPath, err := repotest.GeneratePlugin("dummy_plugin", testDataDir, testPluginName)
+		assert.Nil(t, err)
 
-		err := Add(conf, testPluginName, testRepo)
+		err = Add(conf, testPluginName, pluginPath)
 
 		assert.Nil(t, err, "Expected to be able to add plugin")
 
@@ -149,14 +149,17 @@ func TestAdd(t *testing.T) {
 
 		entries, err := os.ReadDir(pluginDir + "/bin")
 		assert.Nil(t, err)
-		assert.Equal(t, 5, len(entries))
+		assert.Equal(t, 12, len(entries))
 	})
 
 	t.Run("when parameters are valid creates plugin download dir", func(t *testing.T) {
 		testDataDir := t.TempDir()
 		conf := config.Config{DataDir: testDataDir}
 
-		err := Add(conf, testPluginName, testRepo)
+		repoPath, err := repotest.GeneratePlugin("dummy_plugin", testDataDir, testPluginName)
+		assert.Nil(t, err)
+
+		err = Add(conf, testPluginName, repoPath)
 		assert.Nil(t, err)
 
 		// Assert download dir exists
@@ -170,7 +173,10 @@ func TestRemove(t *testing.T) {
 	testDataDir := t.TempDir()
 	conf := config.Config{DataDir: testDataDir}
 
-	err := Add(conf, testPluginName, testRepo)
+	repoPath, err := repotest.GeneratePlugin("dummy_plugin", testDataDir, testPluginName)
+	assert.Nil(t, err)
+
+	err = Add(conf, testPluginName, repoPath)
 	assert.Nil(t, err)
 
 	t.Run("returns error when plugin with name does not exist", func(t *testing.T) {
@@ -197,7 +203,7 @@ func TestRemove(t *testing.T) {
 	})
 
 	t.Run("removes plugin download dir when passed name of installed plugin", func(t *testing.T) {
-		err := Add(conf, testPluginName, testRepo)
+		err := Add(conf, testPluginName, repoPath)
 		assert.Nil(t, err)
 
 		err = Remove(conf, testPluginName)
@@ -214,7 +220,10 @@ func TestUpdate(t *testing.T) {
 	testDataDir := t.TempDir()
 	conf := config.Config{DataDir: testDataDir}
 
-	err := Add(conf, testPluginName, testRepo)
+	repoPath, err := repotest.GeneratePlugin("dummy_plugin", testDataDir, testPluginName)
+	assert.Nil(t, err)
+
+	err = Add(conf, testPluginName, repoPath)
 	assert.Nil(t, err)
 
 	badPluginName := "badplugin"
@@ -359,7 +368,7 @@ func TestRunCallback(t *testing.T) {
 
 	testDataDir := t.TempDir()
 	conf := config.Config{DataDir: testDataDir}
-	testRepo, err := installMockPluginRepo(testDataDir, testPluginName)
+	testRepo, err := repotest.GeneratePlugin("dummy_plugin", testDataDir, testPluginName)
 	assert.Nil(t, err)
 
 	err = Add(conf, testPluginName, testRepo)
@@ -411,126 +420,4 @@ func touchFile(name string) error {
 		return err
 	}
 	return file.Close()
-}
-
-func installMockPluginRepo(dataDir, name string) (string, error) {
-	// Because the legacy dummy plugin directory is relative to the root of this
-	// project I cannot use the usual testing functions to locate it. To
-	// determine the location of it we compute the module root, which also
-	// happens to be the root of the repo.
-	modRootDir, err := moduleRoot()
-	if err != nil {
-		return "", err
-	}
-
-	location := dataDir + "/repo-" + name
-
-	// Then we specify the path to the dummy plugin relative to the module root
-	err = runCmd("cp", "-r", filepath.Join(modRootDir, "test/fixtures/dummy_plugin"), location)
-	if err != nil {
-		return location, err
-	}
-
-	// Definitely some opportunities to refactor here. This code might be
-	// simplified by switching to the Go git library
-	err = runCmd("git", "-C", location, "init", "-q")
-	if err != nil {
-		return location, err
-	}
-
-	err = runCmd("git", "-C", location, "config", "user.name", "\"Test\"")
-	if err != nil {
-		return location, err
-	}
-
-	err = runCmd("git", "-C", location, "config", "user.email", "\"test@example.com\"")
-	if err != nil {
-		return location, err
-	}
-
-	err = runCmd("git", "-C", location, "add", "-A")
-	if err != nil {
-		return location, err
-	}
-
-	err = runCmd("git", "-C", location, "commit", "-q", "-m", fmt.Sprintf("\"asdf %s plugin init\"", name))
-	if err != nil {
-		return location, err
-	}
-
-	err = runCmd("touch", filepath.Join(location, "README.md"))
-	if err != nil {
-		return location, err
-	}
-
-	err = runCmd("git", "-C", location, "add", "-A")
-	if err != nil {
-		return location, err
-	}
-
-	err = runCmd("git", "-C", location, "commit", "-q", "-m", fmt.Sprintf("\"asdf %s plugin readme \"", name))
-	if err != nil {
-		return location, err
-	}
-
-	// kind of ugly but I want a remote with a valid path so I use the same
-	// location as the remote. Probably should refactor
-	err = runCmd("git", "-C", location, "remote", "add", "origin", location)
-	if err != nil {
-		return location, err
-	}
-
-	return location, err
-}
-
-func moduleRoot() (string, error) {
-	currentDir, err := os.Getwd()
-	if err != nil {
-		return "", err
-	}
-
-	return findModuleRoot(currentDir), nil
-}
-
-// Taken from https://github.com/golang/go/blob/9e3b1d53a012e98cfd02de2de8b1bd53522464d4/src/cmd/go/internal/modload/init.go#L1504C1-L1522C2 because that function is in an internal module
-// and I can't rely on it.
-func findModuleRoot(dir string) (roots string) {
-	if dir == "" {
-		panic("dir not set")
-	}
-	dir = filepath.Clean(dir)
-
-	// Look for enclosing go.mod.
-	for {
-		if fi, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil && !fi.IsDir() {
-			return dir
-		}
-		d := filepath.Dir(dir)
-		if d == dir {
-			break
-		}
-		dir = d
-	}
-	return ""
-}
-
-// helper function to make running commands easier
-func runCmd(cmdName string, args ...string) error {
-	cmd := exec.Command(cmdName, args...)
-
-	// Capture stdout and stderr
-	var stdout strings.Builder
-	var stderr strings.Builder
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
-	if err != nil {
-		// If command fails print both stderr and stdout
-		fmt.Println("stdout:", stdout.String())
-		fmt.Println("stderr:", stderr.String())
-		return err
-	}
-
-	return nil
 }
