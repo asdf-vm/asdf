62ab88710e231efc3633f8c9e16f3cfa22459c7b
DeedleFake <deedlefake@users.noreply.github.com>
2024-12-18 17:02:43 -0500

    cmd/asdf: use stamped version info if manual version is not available

--- cmd/asdf/main.go
+++ cmd/asdf/main.go
@@ -1,12 +1,29 @@
 // Main entrypoint for the CLI app
 package main
 
-import "github.com/asdf-vm/asdf/internal/cli"
+import (
+	"runtime/debug"
+
+	"github.com/asdf-vm/asdf/internal/cli"
+)
 
 // Replaced with the real version during a typical build
-var version = "v-dev"
+var version = ""
+
+func getVersion() string {
+	if version != "" {
+		return version
+	}
+
+	info, ok := debug.ReadBuildInfo()
+	if ok {
+		return info.Main.Version
+	}
+
+	return "unknown"
+}
 
 // Placeholder for the real code
 func main() {
-	cli.Execute(version)
+	cli.Execute(getVersion())
 }
